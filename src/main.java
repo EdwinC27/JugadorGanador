2d08a4c79a494a351f1ae6d72a958e01d02dd71b
Castillo Arroyo <kchv321@astrazeneca.net>
2023-01-31 17:08:33 -0600

    Poner ruta y nombre al archivo manualmente

--- src/main.java
+++ src/main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -6,18 +7,26 @@ import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 public class main {
+    static File file;
+    static Scanner scanner = new Scanner(System.in);
+    static Integer ventajaMax = 0;
+    static Integer ganador = 1;
+
+
     public static void main(String[] args) {
         try {
+            abrirArchivo();
+
             // Crea un objeto Scanner para leer el archivo
-            Scanner input = new Scanner(new File("file.txt"));
+            Scanner input = new Scanner(file);
 
             // Lee la cantidad de rondas
             Integer rondas = input.nextInt();
 
             Integer puntosJugador1 = 0;
             Integer puntosJugador2 = 0;
-            Integer ventajaMax = 0;
-            Integer ganador = 1;
+            ventajaMax = 0;
+            ganador = 1;
 
             // verificar las rondas
             if(rondas <= 10000) {
@@ -35,7 +44,7 @@ public class main {
                     }
                 }
 
-                respuestaGanadorArchivo(ganador, ventajaMax);
+                opcGuardarArchivo();
             }
             else {
                 System.out.println("Las rondas superan las esperadas");
@@ -45,9 +54,42 @@ public class main {
         }
     }
 
-    public static void respuestaGanadorArchivo(Integer ganador, Integer ventajaMax) {
+    private static void opcGuardarArchivo() {
+        System.out.println("\nYa se genero la respuesta\n1- Guardar archivo por default\n2- Modificar ruta y nombre\nQue decea: ");
+        Integer opcGuardar = scanner.nextInt();
+
+        if(opcGuardar == 1) { // por defecto
+            try {
+                crearArchivo("respuesta.txt");
+            } catch (Exception e) {
+                System.out.println("Error al crear el archivo");
+            }
+        }
+        else if(opcGuardar == 2) { // eligir ruta y nombre
+            try {
+                scanner.nextLine(); // limpiar bufer
+
+                System.out.println("Ingrese la ruta: ");
+                String ruta = scanner.nextLine();
+
+                System.out.println("Ingrese el nombre: ");
+                String nombre = scanner.nextLine();
+
+                file = new File (ruta,nombre);
+
+                // A partir del objeto File creamos el fichero físicamente
+                if (file.createNewFile()) crearArchivo(String.valueOf(file));
+                else System.out.println("No ha podido ser creado el fichero");
+
+            } catch (Exception e) {
+                System.out.println("Error al crear el archivo");
+            }
+        }
+    }
+
+    public static void crearArchivo(String name) {
         // ruta del archivo a guardar
-        Path path = Paths.get("respuesta.txt");
+        Path path = Paths.get(name);
         String text = ganador + " " + ventajaMax;
 
         try {
@@ -58,5 +100,13 @@ public class main {
         } catch (Exception e) {
             System.out.println("No se pudo  guardar el archivo");
         }
+    }
+
+    private static void abrirArchivo() {
+        // trae el archivo
+        System.out.println("Digite la ruta del archivo a abrir: ");
+        String nombre = scanner.nextLine();
+
+        file = new File(nombre);
     }
 }
