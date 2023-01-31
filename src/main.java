d3b399fd27e4ac40d822af07d1004d861c6afd36
Castillo Arroyo <kchv321@astrazeneca.net>
2023-01-31 11:06:59 -0600

    Checar ganador

--- src/main.java
+++ src/main.java
@@ -1,5 +1,4 @@
 import java.io.File;
-
 import java.util.Scanner;
 
 public class main {
@@ -16,8 +15,27 @@ public class main {
             Integer ventajaMax = 0;
             Integer ganador = 1;
 
-            System.out.println(rondas);
+            // verificar las rondas
+            if(rondas <= 10000) {
+                // lee linea por linea
+                for (int i = 0; i < rondas; i++) {
+                    puntosJugador1 += input.nextInt();
+                    puntosJugador2 += input.nextInt();
 
+                    Integer ventaja = Math.abs(puntosJugador1 - puntosJugador2);
+
+                    // ventaja actual por la ventaja Maxima
+                    if (ventaja > ventajaMax) {
+                        ventajaMax = ventaja;
+                        ganador = puntosJugador1 > puntosJugador2 ? 1 : 2;
+                    }
+                }
+
+                System.out.println(ganador  + " " + ventajaMax);
+            }
+            else {
+                System.out.println("Las rondas superan las esperadas");
+            }
         } catch (Exception e) {
             System.out.println("Error al abrir el archivo");
         }
