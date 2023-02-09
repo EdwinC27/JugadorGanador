4b978efce9aa2744c799fc7eac26e0acd17a5896
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-02-08 20:15:06 -0600

    Sobrescribir el archivo que genero la respuesta si existe

--- src/Guardar.java
+++ src/Guardar.java
@@ -1,21 +1,21 @@
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Guardar {
-    public static void crearArchivo(String name) {
+    public static void crearArchivo(String nombre) {
         // ruta del archivo a guardar
-        Path path = Paths.get(name);
+        File file = new File(nombre);
         String text = Atributos.ganador + " " + Atributos.ventajaMax;
 
         try {
-            Files.write(path, text.getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+            // Sobrescribir los datos existentes
+            Files.write(file.toPath(), text.getBytes(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
 
-            System.out.println("Se creo el archivo");
-        } catch (Exception e) {
-            System.out.println("No se pudo  guardar el archivo");
+            System.out.println("Se creó el archivo");
+        } catch (IOException e) {
+            System.out.println("No se pudo crear el archivo: " + e.getMessage());
         }
     }
 }
-
